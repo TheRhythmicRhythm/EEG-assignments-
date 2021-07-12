9f9b2d09a07b2bc4bd4e98e61912ecf87cf8c5f4
TheRhythmicRhythm <87339325+TheRhythmicRhythm@users.noreply.github.com>
2021-07-12 21:59:46 +0100

    this is the answer to question 2

--- Program.cs
+++ Program.cs
@@ -6,32 +6,71 @@ namespace Rhythmic_Assignment
     {
         static void Main(string[] args)
         {
+            //this is the answer to number 2
 
-            // This is Question 1
-            string student;
-            Console.Write(" are you are full-time or a part-time student:_"); 
-            student = Console.ReadLine();
-            string course;
-            int credit;
+           int a, b, c, d, e, f, n1, n2, n3, x, y;
+                 // first element of the matrix
+                 Console.WriteLine("input first element of the matrix:_");
+                 a  = Convert.ToInt32(Console.ReadLine()); 
+                 Console.WriteLine(a + " is the first element of the matrix");
 
-            if(student == "fulltime"){
-               Console.WriteLine("Whats your course of study:_");
-              course = Console.ReadLine();
-              Console.WriteLine(course + " Wow Thats a Good Course !");   
-            }
-            else if(student == "partime"){
-                Console.WriteLine("How Many Credits Are You Taking:_");
-                 credit  = Convert.ToInt32(Console.ReadLine());
-                 if(credit >= 6){
-                     Console.WriteLine("Too Much Course");
-                 }
-                 else if(credit < 6){
-                     Console.WriteLine("Proceed to the screen");   
-                 }
-                
-            }
-           
-            
+                 // second element of the matrix
+                 Console.WriteLine("input second element of the matrix:_"); 
+                 b  = Convert.ToInt32(Console.ReadLine()); 
+
+                   // third element of the matrix
+                 Console.WriteLine("input third element of the matrix:_"); 
+                 c  = Convert.ToInt32(Console.ReadLine()); 
+
+                   // fourth element of the matrix
+                 Console.WriteLine("input fourth element of the matrix:_"); 
+                 d  = Convert.ToInt32(Console.ReadLine()); 
+
+
+                   // fifth element of the matrix
+                 Console.WriteLine("input fifth element of the matrix:_"); 
+                 e  = Convert.ToInt32(Console.ReadLine()); 
+
+                   // sixth element of the matrix
+                 Console.WriteLine("input sixth element of the matrix:_"); 
+                 f  = Convert.ToInt32(Console.ReadLine()); 
+
+                 // now to find n1
+                  n1 = Convert.ToInt32(a*d - b*c); 
+                  Console.WriteLine(n1);
+                   Console.WriteLine("our n1 is equals to" + n1);
+                   if(n1 == 0){
+                       Console.WriteLine("The Equation has no solution");   
+                   }
+                  
+
+                   // now to find n2
+                  n2 = Convert.ToInt32(e*d - f*c); 
+                  Console.WriteLine(n2);
+                   Console.WriteLine("our n3 is equals to " + n2);
+
+
+
+                 // now to find n3
+                  n3 = Convert.ToInt32(a*f - b*e); 
+                  Console.WriteLine(n3);
+                   Console.WriteLine("our n2 is equals to " + n3);
+
+                   //now to find x 
+                   x = Convert.ToInt32(n2 / n1); 
+                  Console.WriteLine(x);
+                   Console.WriteLine("our x is equals to " + x);
+
+                   //now to find y 
+                   y = Convert.ToInt32(n3 / n1); 
+                  Console.WriteLine(y);
+                   Console.WriteLine("our y is equals to " + y);
+
+
+                   
+                  
+
+
         }
     }
 }
